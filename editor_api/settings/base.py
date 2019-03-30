c6f7de8d6f737816e7f5cfbbf0755bb3998d3ab3
wbdana <william.b.dana@gmail.com>
2019-03-30 13:25:27 -0400

    Configure OAuth2

--- editor_api/settings/base.py
+++ editor_api/settings/base.py
@@ -96,6 +96,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # OAuth2
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -106,10 +110,17 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+
+        # OAuth2
+        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
     )
 }
 
 AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+
+    # OAuth2
     'django.contrib.auth.backends.ModelBackend',
 )
 
